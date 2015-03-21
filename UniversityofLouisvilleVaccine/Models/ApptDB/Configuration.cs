8fe687ae008f2584b528a48f0171b229b64285d1
mhstri01 <mhstri01@louisville.edu>
2015-03-21 11:05:52 -0400

    vaccine clinic changes

---
+++ UniversityofLouisvilleVaccine/Models/ApptDB/Configuration.cs
@@ -0,0 +1,33 @@
+namespace UniversityofLouisvilleVaccine.Models.ApptDB
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversityofLouisvilleVaccine.Models.ApptDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Models\ApptDB";
+            ContextKey = "UniversityofLouisvilleVaccine.Models.ApptDBContext";
+        }
+
+        protected override void Seed(UniversityofLouisvilleVaccine.Models.ApptDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
